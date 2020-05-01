f045eb7a9e1fe9bd1aa191ff1f83571e444c6bff
cameron-bret <c.bret@whiskerhead.com>
2020-05-01 09:21:58 -0400

    added up down arrow to audio table that animates in conjunction with the table opening and closing

--- works.js
+++ works.js
@@ -4,3 +4,19 @@ $(document).ready(function() {
 		$(this).parents().next('.hide').toggle();
 	});
 });
+
+	(function(document){
+		var div = document.getElementById('container');
+		var icon = document.getElementById('icon');
+		var open = false;
+	
+		div.addEventListener('click', function(){
+			if(open){
+				icon.className = 'fa fa-arrow-down';  
+			} else{
+				icon.className = 'fa fa-arrow-down open';
+			}
+	
+			open = !open;
+		});
+	})(document);
